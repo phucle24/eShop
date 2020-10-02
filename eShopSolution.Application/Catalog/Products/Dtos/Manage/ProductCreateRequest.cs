f9ef45e95f1f7b7268d54da245d03ac2a45a9e99
levanphuc <levanphuc24@gmail.com>
2020-10-02 09:24:11 +0700

    Complete application for product

---
+++ eShopSolution.Application/Catalog/Products/Dtos/Manage/ProductCreateRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products.Dtos.Manage
+{
+    public class ProductCreateRequest
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { set; get; }
+        public int Stock { set; get; }
+        public string Description { get; set; }
+        public string Details { get; set; }
+        public string SeoDescription { get; set; }
+        public string SeoTile { get; set; }
+        public int ProductId { get; set; }
+        public string LanguageId { get; set; }
+    }
+}
